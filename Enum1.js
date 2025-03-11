64ffba24c47d8178cdaa71702db05b7bbd9a5d49
Shirisha <shirisha.dharavath@translense.com>
2025-03-11 09:52:01 +0530

    Add files via upload

---
+++ Enum1.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var Direction;
+(function (Direction) {
+    Direction["Up"] = "UP";
+    Direction["Down"] = "DOWN";
+    Direction["Left"] = "LEFT";
+    Direction["Right"] = "RIGHT";
+})(Direction || (Direction = {}));
+var move = Direction.Up;
+console.log(move);
---
+++ basic.ts
@@ -0,0 +1,19 @@
+var myuser = { name: "hitesh", age: 10 };
+console.log("Hitesh");
+//console.log(user.email);
+
+//String enums
+
+enum Direction {
+    Up = 1,
+    Down = 3,
+    Left = 5,
+    Right = 7
+}
+
+let move: Direction = Direction.Up;
+console.log(move);
+
+
+
+
